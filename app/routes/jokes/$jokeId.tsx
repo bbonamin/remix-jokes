2b6ad060643df10155b8372909a94e9b127480c6
Bruno Bonamin <bruno@bonamin.org>
2022-03-23 10:59:34 -0300

    Optimistic UI on create

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,39 +1,27 @@
-import {
-  Form,
-  json,
-  Link,
-  redirect,
-  useCatch,
-  useLoaderData,
-  useParams,
-} from "remix";
+import { json, redirect, useCatch, useLoaderData, useParams } from "remix";
 import { db } from "~/utils/db.server";
 
 import { Prisma } from "@prisma/client";
-import { requireUserId } from "~/utils/session.server";
+import { getUserId, requireUserId } from "~/utils/session.server";
 import type { LoaderFunction, ActionFunction } from "remix";
+import { JokeDisplay } from "~/components/joke";
 
 type JokeWithJokester = Prisma.JokeGetPayload<{ include: { jokester: true } }>;
-type LoaderData = { joke: JokeWithJokester };
+type LoaderData = { joke: JokeWithJokester; isOwner: boolean };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async ({ request, params }) => {
+  const userId = await getUserId(request);
+
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
     include: { jokester: true },
   });
   if (!joke) throw new Response("Joke not found!", { status: 404 });
 
-  const data: LoaderData = { joke };
+  const data: LoaderData = { joke, isOwner: joke.jokesterId === userId };
 
   return json(data);
 };
-
-// export const action: ActionFunction = async ({ request }) => {
-//   // const userId = await getUserId(request);
-//   // const data = useLoaderData<LoaderData>();
-//   // console.log(data);
-//   // const formData = await request.formData();
-// };
 
 export const action: ActionFunction = async ({ request, params }) => {
   const form = await request.formData();
@@ -62,18 +50,7 @@ export const action: ActionFunction = async ({ request, params }) => {
 
 export default function JokeRoute() {
   const data = useLoaderData<LoaderData>();
-  return (
-    <div>
-      <p>Here's your hilarious joke:</p>
-      <p>{data.joke.content}</p>
-      <p>Author: {data.joke.jokester.username}</p>
-      <Link to=".">{data.joke.name} Permalink</Link>
-      <Form method="post">
-        <input type="hidden" name="_method" value="delete" />
-        <button type="submit">Delete</button>
-      </Form>
-    </div>
-  );
+  return <JokeDisplay joke={data.joke} isOwner={data.isOwner} />;
 }
 
 export function ErrorBoundary() {
--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,5 +1,13 @@
-import { ActionFunction, Form, Link, LoaderFunction, useCatch } from "remix";
+import {
+  ActionFunction,
+  Form,
+  Link,
+  LoaderFunction,
+  useCatch,
+  useTransition,
+} from "remix";
 import { useActionData, redirect, json } from "remix";
+import { JokeDisplay } from "~/components/joke";
 import { db } from "~/utils/db.server";
 import { getUserId, requireUserId } from "~/utils/session.server";
 
@@ -70,7 +78,27 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function JokesNewRoute() {
   const actionData = useActionData<ActionData>();
+  const transition = useTransition();
 
+  if (transition.submission) {
+    const formData = transition.submission.formData;
+    const name = formData.get("name");
+    const content = formData.get("content");
+    if (
+      typeof name === "string" &&
+      typeof content === "string" &&
+      !validateJokeContent(content) &&
+      !validateJokeName(name)
+    ) {
+      return (
+        <JokeDisplay
+          joke={{ name, content }}
+          isOwner={true}
+          canDelete={false}
+        />
+      );
+    }
+  }
   return (
     <div>
       <p>Add your own hilarious joke</p>
